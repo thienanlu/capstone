067cdb45c1b64202d314e56096d36177f22d4505
thienanlu <timothy.thienanjw@gmail.com>
2024-12-19 23:10:48 +0700

    5

--- src/App.js
+++ src/App.js
@@ -1,42 +1,34 @@
-// App.js
-import React from 'react';
-import { Routes, Route, Link, HashRouter } from 'react-router-dom';
+// import components
+import Home from './components/Home'
+import About from './components/About'
+import Menu from './components/Menu'
+import Reservation from  './components/Reservation'
+import Order from './components/Order'
+import Login from './components/Login'
+import ConfirmedBooking from './components/ConfirmedBooking'
 
-// Import your components
-import Home from './components/Home';
-import About from './components/About';
-import Menu from './components/Menu';
-import Reservation from './components/Reservation';
-import Order from './components/Order';
-import Login from './components/Login';
-import ConfirmedBooking from './components/ConfirmedBooking';
+// import react-router
+import React from 'react'
+import { Routes, Route, Link, HashRouter } from 'react-router-dom'
+
+// import css
+import './App.css'
 
 function App() {
   return (
-    <div className="main">
-      <HashRouter>
-        {/* <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/menu">Menu</Link></li>
-            <li><Link to="/reservation">Reservation</Link></li>
-            <li><Link to="/order">Order</Link></li>
-            <li><Link to="/login">Login</Link></li>
-            <li><Link to="/confirmation">Confirmation</Link></li>
-          </ul>
-        </nav> */}
+    <>
+      <div className="main">
         <Routes>
-          <Route path="/capstone " element={<Home />} />
-          <Route path="/capstone/about" element={<About />} />
-          <Route path="/capstone/menu" element={<Menu />} />
-          <Route path="/capstone/reservation" element={<Reservation />} />
-          <Route path="/capstone/order" element={<Order />} />
-          <Route path="/capstone/login" element={<Login />} />
-          <Route path="/capstone/confirmation" element={<ConfirmedBooking />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/reservation" element={<Reservation />} />
+          <Route path="/order" element={<Order />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/confirmation" element={<ConfirmedBooking/>} />
         </Routes>
-      </HashRouter>
-    </div>
+      </div>
+    </>
   );
 }
 
